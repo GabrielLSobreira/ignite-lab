616bd743d9952889169bd9cc1b2d108053bcdf64
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-06-24 08:44:50 -0300

    feat(apollo): add environment variables

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o6ojdk0uuf01xi2sl37z84/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
