ee1a0e98d947f7aff57add3838c623895fac4f8d
GabrielLSobreira <gabriellaranjasobreira@gmail.com>
2022-06-24 09:13:40 -0300

    refactor(video): change image in wallpaper card

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,6 +4,7 @@ import {
   CaretRight,
   DiscordLogo,
   FileArrowDown,
+  Image,
   Lightning,
   Rocket,
 } from 'phosphor-react';
@@ -140,7 +141,7 @@ export function Video(props: VideoProps) {
             className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
           >
             <div className="bg-green-700 h-full p-6 flex items-center">
-              <FileArrowDown size={40} />
+              <Image size={40} />
             </div>
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">Wallpapers exclusivos</strong>
